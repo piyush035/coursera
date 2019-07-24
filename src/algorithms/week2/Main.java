b541f6b9274f39933d2dbf63bfe54c83c85beda9
Piyush Kumar Gupta <piyush035@gmail.com>
2019-07-24 12:53:55 +0530

    Test

---
+++ src/algorithms/week2/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws  IOException {
+        FixedCapacityStackOfStrings stack = new FixedCapacityStackOfStrings();
+        for (String s: args) {
+            if("-".equals(s)){
+                System.out.println(stack.pop());
+                System.out.println("Size :: "+ stack.size());
+            }else {
+                stack.push(s);
+                System.out.println("Size :: "+ stack.size());
+            }
+        }
+    }
+}
