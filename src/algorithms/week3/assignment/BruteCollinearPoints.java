9fef7e56253ca6e0b18aa10d01df60e78acf9394
Piyush Gupta <piyush035@gmail.com>
2019-08-04 19:41:05 +0530

    Updated for week 3 assignment

--- src/algorithms/week3/assignment/BruteCollinearPoints.java
+++ src/algorithms/week3/assignment/BruteCollinearPoints.java
@@ -1,4 +1,68 @@
 package algorithms.week3.assignment;
 
+import java.util.Arrays;
+
+/******************************************************************************
+ *  Compilation:  javac Point.java
+ *  Execution:    java Point
+ *  Dependencies: none
+ *
+ *  An immutable data type for points in the plane.
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ ******************************************************************************/
+
 public class BruteCollinearPoints {
+
+    private LineSegment[] lineSegments;
+
+    private int n = 0;
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null) throw new IllegalArgumentException();
+        if (checkForDuplicateOrNull(points)) throw new IllegalArgumentException();
+        lineSegments = new LineSegment[1];
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                for (int k = j + 1; k < points.length; k++) {
+                    for (int l = k + 1; l < points.length; l++) {
+                        if (points[i].slopeTo(points[j]) == points[i].slopeTo(points[k]) && points[i].slopeTo(points[j]) == points[i].slopeTo(points[l])) {
+                            if (lineSegments.length == n) resize(2 * lineSegments.length);
+                            Point[] point = new Point[]{points[i], points[j], points[k], points[l]};
+                            Arrays.sort(point);
+                            lineSegments[n++] = new LineSegment(point[0], point[3]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void resize(int newSize) {
+        LineSegment[] copyLineSegments = new LineSegment[newSize];
+        for (int i = 0; i < lineSegments.length; i++) {
+            copyLineSegments[i] = lineSegments[i];
+        }
+        lineSegments = copyLineSegments;
+    }
+
+    private boolean checkForDuplicateOrNull(Point[] points) {
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i] == null || points[j] == null || points[i].compareTo(points[j]) == 0) return true;
+            }
+        }
+        return false;
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return lineSegments.length;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return lineSegments;
+    }
 }
